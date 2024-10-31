70a95376a6e221fc976d53d6cd80c9fbd6f79239
musicos-da <musicosdaditadura@gmail.com>
2024-10-30 22:30:57 -0400

    Add files via upload

---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'fala pessoal'
+exports.bemvindo = 'bem vindo ao node'
+module.exports.atelogo = 'até proximo exemplo'
---
+++ node/moduloB.js
@@ -0,0 +1,6 @@
+module.exports = {
+    bomdia: 'bom dia',
+    boanoite(){
+        return 'boa noite'
+    }
+}
